26ede4af8540dea4429b5ddf54cc696f4f374a65
garretbs <garretbs@gmail.com>
2018-10-03 12:27:26 -0400

    Edit past end of string success

--- paper.rb
+++ paper.rb
@@ -14,7 +14,7 @@ class Paper
 		return if position.nil?
 		@message = @message.reverse.split(text.reverse, 2).join(" "*text.length).reverse
 		if edit_text
-			end_position = position + [@message.length, edit_text.length].min - 1
+			end_position = [@message.length, position+edit_text.length].min - 1
 			edit_text_position = -1
 			for i in position..end_position
 				edit_text_position += 1

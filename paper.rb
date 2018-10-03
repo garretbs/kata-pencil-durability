eb815481d2d073ad47b634783e7294a67ee64d0f
garretbs <garretbs@gmail.com>
2018-10-03 12:22:27 -0400

    Edit collision test success

--- paper.rb
+++ paper.rb
@@ -14,10 +14,11 @@ class Paper
 		return if position.nil?
 		@message = @message.reverse.split(text.reverse, 2).join(" "*text.length).reverse
 		if edit_text
-			end_position = position + [text.length, edit_text.length].min - 1
+			end_position = position + [@message.length, edit_text.length].min - 1
 			edit_text_position = -1
 			for i in position..end_position
-				@message[i] = edit_text[edit_text_position += 1]
+				edit_text_position += 1
+				@message[i] = @message[i] =~ /\s/ ? edit_text[edit_text_position] : "@"
 			end
 		end
 	end
